ae978b830f4f96931cee2e9147810370b1068876
TejasPatel3300 <77225883+TejasPatel3300@users.noreply.github.com>
2022-01-22 19:16:33 +0530

    feat: font-files and packages added

---
+++ lib/constants/colors.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color bgColorLightTop = Color(0xFFD7D5E4);
+  static const Color bgColorLightBottom = Color(0xFF7A7886);
+  static const Color bgColorDarkTop = Color(0xFF62696E);
+  static const Color bgColorDarkBottom = Color(0xFF2A1D29);
+
+  static const Color textColorLight = Color(0xFFD7D5E4);
+  static const Color textColorDark = Color(0xFF2A1D29);
+
+  static const Color themeColorLight = Color(0xFF00B0AA);
+  static const Color themeColorDark = Color(0xFF005855);
+
+  static const Gradient gradientLightMode = LinearGradient(
+    colors: [bgColorLightTop, bgColorLightBottom],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+  );
+
+  static const Gradient gradientDarkMode = LinearGradient(
+    colors: [bgColorDarkTop, bgColorDarkBottom],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+  );
+}
